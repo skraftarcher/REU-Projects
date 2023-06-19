4ca23419e287957545b65153301230c9d60de8c4
skraftarcher <sarcher@lumcon.edu>
2023-06-19 11:37:24 -0500

    figure examples

--- scripts/stephanie_trial.R
+++ scripts/stephanie_trial.R
@@ -28,10 +28,13 @@ sg19$Site<-factor(sg19$Site,level=c("HC","TC","SC","MOW","CA","HT","LH"))
 ggplot(data=sg19)+
 #  geom_vline(aes(xintercept=0),size=3,color="purple")+
   #geom_boxplot(aes(y=Cnpy1Tt,fill=Site,color=Site))+
-   geom_jitter(aes(x=Dnsty1Tt,y=Cnpy2Tt,color=Cnpy3Tt),size=4,width = .1,alpha=.5)+
+   geom_jitter(aes(x=Site,y=Cnpy2Tt,color=Cnpy3Tt,size=Tt),shape=17,width = .1,alpha=.2)+
+  #geom_jitter(aes(x=Dnsty1Tt,y=Cnpy2Tt,color=Site),size=4,width = .1,alpha=.5)+
   #geom_point(aes(x=Dnsty1Tt,y=Cnpy2Tt,color=Cnpy3Tt),size=4,alpha=.3)+
-  #scale_fill_viridis_d(option="A",end=.8,begin=.2)+
-  scale_color_viridis_c(option="B",end=.8)
+  #scale_color_viridis_d(option="A",end=.8,begin=.2)#+
+  scale_color_viridis_c(option="B",end=.8)+
+  ylab("This is an ugly figure")+
+  theme(legend.position = "top")
 
 # creating a list and working with them.----
 sglist<-list(x=data.frame(a=1,b=2),y="this is part of the list",z=c(1,2,3))
